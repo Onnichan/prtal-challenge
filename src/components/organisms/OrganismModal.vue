b3fd04e62380f2c53b064c1f8468c56df9bbc330
Daniel Aguilar <dyangeltk17@gmail.com>
2022-10-09 09:09:53 -0500

    :lipstick: Add backdrop to the model

--- src/components/organisms/OrganismModal.vue
+++ src/components/organisms/OrganismModal.vue
@@ -23,26 +23,28 @@ export default {
 </script>
 <template>
   <transition name="modal">
-    <div class="modal">
-      <div class="modal-mask"></div>
-      <div class="modal__header">
-        <AtomText tag="h2">Modificar estado</AtomText>
-      </div>
-      <div class="modal__body">
-        <AtomText>Selecciona el estado en que se encuentra el pago</AtomText>
-        <label v-bind:style="{ margin: '10px 0' }">Estado </label>
-        <AtomSelect
-          v-bind:options="states"
-          v-bind:style="{ width: '37%' }"
-        ></AtomSelect>
-      </div>
-      <div class="modal__footer">
-        <MoleculeIconButton>
-          <AtomDeleteIcon />
-        </MoleculeIconButton>
-        <AtomButton class="bg-primary-500" @click="setOpen(false)"
-          >Guardar</AtomButton
-        >
+    <div class="modal-container">
+      <div class="modal-mask" @click="setOpen(false)"></div>
+      <div class="modal">
+        <div class="modal__header">
+          <AtomText tag="h2">Modificar estado</AtomText>
+        </div>
+        <div class="modal__body">
+          <AtomText>Selecciona el estado en que se encuentra el pago</AtomText>
+          <label v-bind:style="{ margin: '10px 0' }">Estado </label>
+          <AtomSelect
+            v-bind:options="states"
+            v-bind:style="{ width: '37%' }"
+          ></AtomSelect>
+        </div>
+        <div class="modal__footer">
+          <MoleculeIconButton>
+            <AtomDeleteIcon />
+          </MoleculeIconButton>
+          <AtomButton class="bg-primary-500" @click="setOpen(false)"
+            >Guardar</AtomButton
+          >
+        </div>
       </div>
     </div>
   </transition>
